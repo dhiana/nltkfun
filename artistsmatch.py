015d9fb4115dfe12b87455347a5efa49a13e7070
Dhiana Deva <dhiana.deva@gmail.com>
2014-11-05 09:46:10 -0200

    Adds Artist matching

---
+++ artistsmatch.py
@@ -0,0 +1,62 @@
+import os
+import json
+import re
+import string
+import requests
+from nltk import word_tokenize
+from nltk.util import ngrams
+from nltk.metrics.distance import edit_distance, jaccard_distance
+
+regex = re.compile('[%s]' % re.escape(string.punctuation))
+
+def preproc(s):
+    s = s.lower()
+    return s
+
+def is_match(metric):
+    if (metric < 0.7):
+        return True
+    return False
+
+
+DEBUG = False
+if DEBUG:
+    json_file = open('data/Bohemian-Rhapsody.json')
+    json_response = json.load(json_file)
+    json_file.close()
+else:
+    url = 'http://tinysong.com/s/You-Give-Love-A-Bad-Name?format=json&key=%s' % os.environ.get('TINYSONG_KEY')
+    r = requests.get(url)
+    json_response = r.json()
+
+# Features
+top_entry = json_response[0]
+
+FEATURE = 'ArtistName'
+NGRAMS = 2
+top_entry_value = preproc(top_entry[FEATURE])
+print 'Comparing song name to top match reference:', top_entry[FEATURE]
+top_entry_char_bigrams = set(ngrams(top_entry_value, NGRAMS))
+top_entry_word_bigrams = set(ngrams(word_tokenize(top_entry_value), NGRAMS))
+
+for song in json_response[1:]:
+
+    this_value = preproc(song[FEATURE])
+    print '\t%s' % song[FEATURE]
+
+    l_distance = edit_distance(top_entry_value, this_value)
+    print '\t\tLevenshtein:\t\t'+str(l_distance)
+
+    ld_distance = edit_distance(top_entry_value, this_value, True)
+    print '\t\tLevenshtein-Damerau:\t'+str(ld_distance)
+
+    this_char_bigrams = set(ngrams(this_value, NGRAMS))
+    cbg_distance = jaccard_distance(top_entry_char_bigrams, this_char_bigrams)
+    print '\t\tChar bigrams + Jaccard:\t'+str(cbg_distance)
+
+    this_word_bigrams = set(ngrams(word_tokenize(this_value), NGRAMS))
+    if len(this_word_bigrams) and len(top_entry_word_bigrams):
+        wbg_distance = jaccard_distance(top_entry_word_bigrams, this_word_bigrams)
+        print '\t\tWord bigrams + Jaccard:\t'+str(wbg_distance)
+        is_this_match = is_match(wbg_distance)
+        print '\t\tMatch?', is_this_match
